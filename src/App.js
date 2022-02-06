c8125105264e62b3a270ba31349658d340f5840e
medzviadziou <medzviadziou@gmail.com>
2022-02-07 01:05:06 +0300

    create advantage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/header/header";
 import Preview from "./components/preview/preview";
 
 import './App.scss';
+import Advantage from "./components/advantage/advantage";
 
 function App() {
   return (
     <section className="App">
       <Header/>
       <Preview />
+      <Advantage />
     </section>
   );
 }
---
+++ src/components/advantage/advantage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import truck from './img/truck.svg'
+import refresh from './img/refresh.svg'
+import support from './img/support.svg'
+
+import './advantage.scss'
+
+const Advantage = () => {
+    return (
+        <div className='advantage'>
+            <div className='advantage__block'>
+                <img className='advantage__img' src={truck} alt=""/>
+                <div className='advantage__description'>
+                    <h3 className='advantage__title'>FREE SHIPPING</h3>
+                    <p className='advantage__text'>On all UA order or order above $100</p>
+                </div>
+            </div>
+            <div className='advantage__block'>
+                <img  className='advantage__img' src={refresh} alt=""/>
+                <div className='advantage__description'>
+                    <h3 className='advantage__title'>30 DAYS RETURN</h3>
+                    <p className='advantage__text'>Simply return it within 30 days for an exchange</p>
+                </div>
+            </div>
+            <div className='advantage__block'>
+                <img className='advantage__img' src={support} alt=""/>
+                <div className='advantage__description'>
+                    <h3 className='advantage__title'>SUPPORT 24/7</h3>
+                    <p className='advantage__text'>Contact us 24 hours a day, 7 days a week</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Advantage;
