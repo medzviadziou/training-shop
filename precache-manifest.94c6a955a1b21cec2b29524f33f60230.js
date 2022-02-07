801b1ffff44dd38ba264c7e39f713de819b6836c
medzviadziou <98909549+medzviadziou@users.noreply.github.com>
2022-02-07 22:18:27 +0000

    Deploying to gh-pages from @ medzviadziou/training-shop@6bea0b3ea62336e22e30dccfe281e9aa4832f662 🚀

---
+++ precache-manifest.94c6a955a1b21cec2b29524f33f60230.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5408599d2ca5a225ccf2de6ebeaed31f",
+    "url": "/training-shop/index.html"
+  },
+  {
+    "revision": "17b20c2705cde71e1210",
+    "url": "/training-shop/static/css/main.70a96721.chunk.css"
+  },
+  {
+    "revision": "34b689c880b0c349f19d",
+    "url": "/training-shop/static/js/2.b16c5408.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/training-shop/static/js/2.b16c5408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b20c2705cde71e1210",
+    "url": "/training-shop/static/js/main.03767453.chunk.js"
+  },
+  {
+    "revision": "5068c357bca5672e3a8d",
+    "url": "/training-shop/static/js/runtime-main.461599c6.js"
+  },
+  {
+    "revision": "278ce0e7c60a72f2dad853993a47388f",
+    "url": "/training-shop/static/media/cart.278ce0e7.svg"
+  },
+  {
+    "revision": "7740b019335207ecdee1c0ae28d10cc2",
+    "url": "/training-shop/static/media/clock.7740b019.svg"
+  },
+  {
+    "revision": "c27e33a0f535dd256ace6e0b5cf5f63d",
+    "url": "/training-shop/static/media/facebook.c27e33a0.svg"
+  },
+  {
+    "revision": "40915ca3f04f2261f78d1cf27d631fb6",
+    "url": "/training-shop/static/media/globe.40915ca3.svg"
+  },
+  {
+    "revision": "7113ea34142b95ad119e20041550d963",
+    "url": "/training-shop/static/media/instagram.7113ea34.svg"
+  },
+  {
+    "revision": "5215ab224f7290074f24ea8a5a92b55a",
+    "url": "/training-shop/static/media/location.5215ab22.svg"
+  },
+  {
+    "revision": "2af24722870868a5e0f6b792ed71109f",
+    "url": "/training-shop/static/media/logo.2af24722.svg"
+  },
+  {
+    "revision": "440b7628ff09259602cef2f320e136e2",
+    "url": "/training-shop/static/media/next.440b7628.svg"
+  },
+  {
+    "revision": "c0f3d819b1484259fa2f22fdde5953a0",
+    "url": "/training-shop/static/media/phone.c0f3d819.svg"
+  },
+  {
+    "revision": "a16aa5e337dadf9d19ee8760412f257e",
+    "url": "/training-shop/static/media/pinterest.a16aa5e3.svg"
+  },
+  {
+    "revision": "11d179f1363a28b6dd42de81c06b8b72",
+    "url": "/training-shop/static/media/preview_accessories.11d179f1.jpg"
+  },
+  {
+    "revision": "e90d3b3ee059b02a75274cf08788c453",
+    "url": "/training-shop/static/media/preview_men.e90d3b3e.jpg"
+  },
+  {
+    "revision": "5bbb75e663be454d7bf2d31e61cab8b7",
+    "url": "/training-shop/static/media/preview_women.5bbb75e6.jpg"
+  },
+  {
+    "revision": "ba2ba65b2a0535b9c3a6247046f89ec7",
+    "url": "/training-shop/static/media/previous.ba2ba65b.svg"
+  },
+  {
+    "revision": "413a338fc4e4168516fb6ec15c68111f",
+    "url": "/training-shop/static/media/refresh.413a338f.svg"
+  },
+  {
+    "revision": "4c728ef62bbf8080cb04e7e84ca5c382",
+    "url": "/training-shop/static/media/search.4c728ef6.svg"
+  },
+  {
+    "revision": "0e00750f921c886d5362e89b73dd3eb4",
+    "url": "/training-shop/static/media/slider.0e00750f.jpg"
+  },
+  {
+    "revision": "801c531eba629c586161fcf57f3da916",
+    "url": "/training-shop/static/media/support.801c531e.svg"
+  },
+  {
+    "revision": "08cd335e39c2c9e3b0b31ad08c7d91ad",
+    "url": "/training-shop/static/media/truck.08cd335e.svg"
+  },
+  {
+    "revision": "4063ebfb5a2b82ea801c32c51aab1fda",
+    "url": "/training-shop/static/media/twitter.4063ebfb.svg"
+  },
+  {
+    "revision": "81378b8faa6dd352fe95d777281e408f",
+    "url": "/training-shop/static/media/user.81378b8f.svg"
+  }
+]);
