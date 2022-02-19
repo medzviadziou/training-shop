832d6e97c6f523356ccef99144d67ef614ce56f8
medzviadziou <medzviadziou@gmail.com>
2022-02-20 01:43:45 +0300

    menu-burger

--- src/components/panel/index.jsx
+++ src/components/panel/index.jsx
@@ -21,26 +21,28 @@ const Panel = () => {
     }
 
     const clickHandler = () => {
+        if(!isMenuOpen){
+            document.body.style.overflow = 'hidden';
+        }
         toggleMenuMode();
     };
     const clickClose = () => {
         if (isMenuOpen) {
+            document.body.style.overflow = '';
             toggleMenuMode();
         }
     }
 
     return (
         <div className='panel contain' onClick={clickClose}>
-            <Link to="/" data-test-id='header-logo-link'><img className='panel__logo' src={logo}
-                                                              alt="CleverShop"/></Link>
+            <Link to="/" data-test-id='header-logo-link'><img className='panel__logo' src={logo} alt="CleverShop"/></Link>
             <Nav isMenuOpen={isMenuOpen}/>
             <ul className='panel__block'>
                 <li className='panel__ico'><img src={search} alt="search" width="24" height="24"/></li>
                 <li className='panel__ico'><img src={globe} alt="globe" width="24" height="24"/></li>
                 <li className='panel__ico'><img src={user} alt="user" width="24" height="24"/></li>
                 <li className='panel__ico panel__cart'><img src={cart} alt="cart" width="24" height="24"/></li>
-                <li className='panel__meu-burger' onClick={clickHandler}><span
-                    className={classNames('panel__burger', {'panel__burger--open': isMenuOpen})}> </span></li>
+                <li className='panel__meu-burger' onClick={clickHandler}><span className={classNames('panel__burger', {'panel__burger--open': isMenuOpen})}> </span></li>
             </ul>
         </div>
     );
