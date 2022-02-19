8d4e3808444b6b20a1c2734d6a5e3b17afd28a03
medzviadziou <medzviadziou@gmail.com>
2022-02-19 19:24:56 +0300

    create menu-burger & cross

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './nav.scss'
 import {Link} from "react-router-dom";
 import MENU from "../../data/menuList";
+import classNames from "classnames";
 
 const Nav = () => {
+
+
     return (
         <nav className='nav'>
-            <ul className='nav__list' data-test-id='menu'>
+            <div className={classNames('nav__overlay', {'nav__overlay--open':false})}></div>
+            <ul className={classNames('nav__list', {'nav__list--open':false})} data-test-id='menu'>
                 {MENU.map(({id,name,path}) => {
                     return <li className='nav__item'><Link key={id} to={`/${path}`} data-test-id={`menu-link-${path}`}>{name}</Link></li>
                 })}
--- src/components/panel/index.jsx
+++ src/components/panel/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link,} from "react-router-dom";
 import './panel.scss'
 
@@ -9,18 +9,31 @@ import user from "./img/user.svg";
 import cart from "./img/cart.svg";
 
 import Nav from "../nav";
+import classNames from "classnames";
 
 
 const Panel = () => {
+
+
+    const [isMenuOpen, toggleMenu] = useState(false)
+
+    function toggleMenuMode () {
+        toggleMenu(!isMenuOpen)
+    }
+    const clickHandler = () => {
+        toggleMenuMode();
+    };
+
     return (
         <div className='panel contain'>
             <Link to="/" data-test-id='header-logo-link'><img className='panel__logo' src={logo} alt="CleverShop"/></Link>
             <Nav className='panel__nav' />
             <ul className='panel__block'>
-                <li className='panel__ico'><img src={search} alt="search"/></li>
-                <li className='panel__ico'><img src={globe} alt="globe"/></li>
-                <li className='panel__ico'><img src={user} alt="user"/></li>
-                <li className='panel__ico panel__cart'><img src={cart} alt="cart"/></li>
+                <li className='panel__ico'><img src={search} alt="search" width="24" height="24" /></li>
+                <li className='panel__ico'><img src={globe} alt="globe" width="24" height="24" /></li>
+                <li className='panel__ico'><img src={user} alt="user" width="24" height="24" /></li>
+                <li className='panel__ico panel__cart'><img src={cart} alt="cart" width="24" height="24" /></li>
+                <li className='panel__meu-burger'><span className={classNames('panel__burger', { 'panel__burger--open': isMenuOpen })} onClick={clickHandler}> </span></li>
             </ul>
         </div>
     );
