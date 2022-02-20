7b91db2e50b825bce191c6905b9247ddb0ac2fd9
medzviadziou <medzviadziou@gmail.com>
2022-02-21 01:35:52 +0300

    update menu-burger

--- src/components/panel/index.jsx
+++ src/components/panel/index.jsx
@@ -20,31 +20,42 @@ const Panel = () => {
         toggleMenu(!isMenuOpen)
     }
 
-    const clickHandler = () => {
-        if(!isMenuOpen){
-            document.body.style.overflow = 'hidden';
-        }
-        toggleMenuMode();
-    };
-    const clickClose = () => {
-        if (isMenuOpen) {
-            document.body.style.overflow = '';
+    const panel = document.querySelector('div');
+
+    panel.onclick = function (event) {
+        let target = event.target;
+        if (target.tagName === 'BUTTON' || "SPAN") {
+            if (!isMenuOpen) {
+                document.body.style.overflow = 'hidden';
+            }
             toggleMenuMode();
         }
-    }
+        if (target.tagName !== 'BUTTON' || "SPAN") {
+            if (isMenuOpen) {
+                document.body.style.overflow = '';
+                toggleMenuMode();
+            }
+        }
+    };
 
     return (
 
-
-        <div className='panel contain' onClick={clickClose}>
-            <Link to="/" data-test-id='header-logo-link'><img className='panel__logo' src={logo} alt="CleverShop"/></Link>
+        <div className='panel contain'>
+            <Link to="/" data-test-id='header-logo-link'><img className='panel__logo' src={logo}
+                                                              alt="CleverShop"/></Link>
             <Nav isMenuOpen={isMenuOpen}/>
             <ul className='panel__block'>
                 <li className='panel__ico'><img src={search} alt="search" width="24" height="24"/></li>
                 <li className='panel__ico'><img src={globe} alt="globe" width="24" height="24"/></li>
                 <li className='panel__ico'><img src={user} alt="user" width="24" height="24"/></li>
                 <li className='panel__ico panel__cart'><img src={cart} alt="cart" width="24" height="24"/></li>
-                <li className='panel__meu-burger' onClick={clickHandler}><span className={classNames('panel__burger', {'panel__burger--open': isMenuOpen})}> </span></li>
+                <li className='panel__ico'>
+                    <button className='panel__menu-burger'>
+                        <span className={classNames('panel__burger', {'panel__burger--1': isMenuOpen})}> </span>
+                        <span className={classNames('panel__burger', {'panel__burger--2': isMenuOpen})}> </span>
+                        <span className={classNames('panel__burger', {'panel__burger--3': isMenuOpen})}> </span>
+                    </button>
+                </li>
             </ul>
         </div>
     );
