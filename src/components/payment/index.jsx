eade20532a2738c93b2208789b4504c13f2be8fb
medzviadziou <medzviadziou@gmail.com>
2022-04-05 22:26:13 +0300

    first commit in  branch 'main-2'

---
+++ src/components/payment/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './payment.scss'
+
+const Payment = () => {
+    return (
+        <div className='payment'>
+
+        </div>
+    );
+};
+
+export default Payment;
